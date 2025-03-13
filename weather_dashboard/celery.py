2202f1eb320415fb6f838a7f03c451fab5b3fc01
Mykyta <112129014+Nikita10Polyakov@users.noreply.github.com>
2025-03-13 15:18:37 +0200

    Update celery.py

--- weather_dashboard/celery.py
+++ weather_dashboard/celery.py
@@ -1,15 +1,12 @@
 import os
 from celery import Celery
 
-# Встановлюємо змінну середовища для налаштувань Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather_dashboard.settings')
 
 app = Celery('weather_dashboard')
 
-# Завантажуємо конфігурацію Celery із settings.py
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Автоматично знаходить завдання у всіх додатках
 app.autodiscover_tasks()
 
 @app.task(bind=True)
