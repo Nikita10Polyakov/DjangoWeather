e9add146e4a3f20163ab479cf9a0bb9ed6fefc97
Mykyta <112129014+Nikita10Polyakov@users.noreply.github.com>
2025-03-13 15:21:05 +0200

    Update api_urls.py

--- weather/api_urls.py
+++ weather/api_urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import WeatherDataList, WeatherDataByCity, update_weather_api
 
 urlpatterns = [
-    path('weather/', WeatherDataList.as_view(), name='weather-api'),  # Отримати всі записи погоди
-    path('weather/<str:city>/', WeatherDataByCity.as_view(), name='weather-api-city'),  # Отримати погоду по місту
-    path('update/', update_weather_api, name='update-weather-api'),  # Оновити погоду вручну
+    path('weather/', WeatherDataList.as_view(), name='weather-api'),  
+    path('weather/<str:city>/', WeatherDataByCity.as_view(), name='weather-api-city'),  
+    path('update/', update_weather_api, name='update-weather-api'),  
 ]
