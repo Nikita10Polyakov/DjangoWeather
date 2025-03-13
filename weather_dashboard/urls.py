dfb1e576f4ebc4a3f8d3de0e24dc1cbf8634139d
Mykyta <112129014+Nikita10Polyakov@users.noreply.github.com>
2025-03-13 15:20:16 +0200

    Update urls.py

--- weather_dashboard/urls.py
+++ weather_dashboard/urls.py
@@ -4,8 +4,8 @@ from weather.views import weather_view, update_weather_api, WeatherDataList, Wea
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('weather/', include('weather.urls')),  # Головна сторінка з погодою
-    path('api/weather/', WeatherDataList.as_view(), name='weather-api'),  # API для всіх записів погоди
-    path('api/weather/<str:city>/', WeatherDataByCity.as_view(), name='weather-api-city'),  # API для конкретного міста
-    path('api/update/', update_weather_api, name='update-weather-api'),  # API для ручного оновлення погоди
+    path('weather/', include('weather.urls')), 
+    path('api/weather/', WeatherDataList.as_view(), name='weather-api'),  
+    path('api/weather/<str:city>/', WeatherDataByCity.as_view(), name='weather-api-city'),  
+    path('api/update/', update_weather_api, name='update-weather-api'),  
 ]
